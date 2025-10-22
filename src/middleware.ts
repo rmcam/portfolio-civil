4d3cd937c6bdd20c823ae9e52ad246b5feb7bc63
rmcam <rmcam.dev@gmail.com>
2025-10-22 13:16:44 -0500

    Upload in hostinger

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 // src/middleware.ts
 import { defineMiddleware } from "astro:middleware";
-import { Redis } from "@upstash/redis";
+// import { Redis } from "@upstash/redis";
 
-const redis = new Redis({
-  url: import.meta.env.UPSTASH_REDIS_REST_URL,
-  token: import.meta.env.UPSTASH_REDIS_REST_TOKEN,
-});
+// const redis = new Redis({
+//   url: import.meta.env.UPSTASH_REDIS_REST_URL,
+//   token: import.meta.env.UPSTASH_REDIS_REST_TOKEN,
+// });
 
 export const onRequest = defineMiddleware(async (context, next) => {
   // Solo incrementamos el contador para las páginas, no para assets (css, js, etc.)
@@ -13,16 +13,17 @@ export const onRequest = defineMiddleware(async (context, next) => {
     return next();
   }
 
-  try {
-    // Incrementa el contador y obtén el nuevo valor
-    const views = await redis.incr("views");
-    // Adjuntamos el contador al objeto `locals` para que esté disponible en todas las páginas
-    context.locals.viewCount = views;
-  } catch (error) {
-    console.error("Error connecting to Upstash Redis:", error);
-    // Si hay un error, podemos asignar un valor por defecto
-    context.locals.viewCount = 0;
-  }
+  // try {
+  //   // Incrementa el contador y obtén el nuevo valor
+  //   const views = await redis.incr("views");
+  //   // Adjuntamos el contador al objeto `locals` para que esté disponible en todas las páginas
+  //   context.locals.viewCount = views;
+  // } catch (error) {
+  //   console.error("Error connecting to Upstash Redis:", error);
+  //   // Si hay un error, podemos asignar un valor por defecto
+  //   context.locals.viewCount = 0;
+  // }
+  context.locals.viewCount = 0; // Asignar un valor por defecto si no se usa Redis
 
   // Continuamos con la renderización de la página
   return next();
