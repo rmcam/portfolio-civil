d0f4f24c22033ce1724a1128a15018cb0e3abc1f
rmcam <rmcam.dev@gmail.com>
2025-10-22 13:27:52 -0500

    New changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static',
-  trailingSlash: 'always'
+  integrations: [tailwind()]
 });
